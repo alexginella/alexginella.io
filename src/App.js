2945cedcceda8f611bb507f271716eb99d9f7755
Alex Ginella <ginellalex@gmail.com>
2020-07-07 23:21:58 -0700

    making a change to test deployment

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          RIP Bob Smith
         </a>
       </header>
     </div>
