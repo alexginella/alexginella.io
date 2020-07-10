b2216c4572fa2d4d1b1fe1d2a7413189b0b049de
Alex Ginella <ginellalex@gmail.com>
2020-07-09 18:23:18 -0700

    test instagram clone idk why

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Post from './components/Post'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          RIP Bob Smith
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render(){
+    return ( 
+      <div className="App">
+        <Header/>
+        <section className="App-main">
+          <Post username="ginillabean" avatar="https://www.chicagotribune.com/resizer/Pl_EhctmVe873qZZT1znFVQ5bMo=/fit-in/800x533/smart/filters:fill(black)/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/QWGWSDYK3BE7ZBWURXXJ7AQ6MM.jpg" caption="just 4 da memes (& da trap)" image="https://www.gannett-cdn.com/-mm-/62035649379a678fb24f5dbe5a4b66cba0a995c9/c=179-0-2659-3307/local/-/media/2016/05/29/Cincinnati/Cincinnati/636001135964333349-Harambe2.jpg" />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
