d85c82810c77904a959040d8fab9f1df80c92559
Alex Ginella <ginellalex@gmail.com>
2020-07-22 15:59:07 -0700

    tried better centering the resume using cells

--- src/components/resume.js
+++ src/components/resume.js
@@ -12,8 +12,9 @@ class Resume extends Component {
                         <a style={{fontSize:"20pt"}} className="welcome-text" rel="stylesheet" href="https://docs.google.com/document/d/182ofZSJR9y4GEHKDQK8B3WR5QzKYTG8KA8JEl5Pm0zg/edit?usp=sharing">Shareable Link</a>
                     </Cell>
                     <Cell style={{textAlign:"center"}} col={6}>                        
-                    <iframe style={{width:"850px", height:"900px",frameborder:"0"}} src="https://docs.google.com/document/d/e/2PACX-1vR_gP_l_4bwD66HUFoHKcHQgDUNITgYvHeU4_BGQ0YC5tWQ5DwpwyxRuaUXhUnysU6BLGPYgnRnIlW3/pub?embedded=true"></iframe>
+                        <iframe style={{width:"850px", height:"900px",frameborder:"0"}} src="https://docs.google.com/document/d/e/2PACX-1vR_gP_l_4bwD66HUFoHKcHQgDUNITgYvHeU4_BGQ0YC5tWQ5DwpwyxRuaUXhUnysU6BLGPYgnRnIlW3/pub?embedded=true"></iframe>
                     </Cell>
+                    <Cell col={3}></Cell>
                 </Grid>
             </div>
         )
