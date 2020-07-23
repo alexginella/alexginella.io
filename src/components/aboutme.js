c05bee876aefd2a0567a3c2d3281147caa08d009
Alex Ginella <ginellalex@gmail.com>
2020-07-22 19:01:21 -0700

    fixed aboutme page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -5,9 +5,12 @@ class AboutMe extends Component {
     render(){
         return (
             <Grid>
+                <Cell col={12}>
+                    <h1 className="welcome-text">Who am I?</h1>
+                </Cell>
                 <Cell col={3}/>
                 <Cell col={6}>
-                    <p className="intro-text">Alex Ginella is a senior Computer Science and Linguistics undergraduate at
+                    <p className="about-me">Alex Ginella is a senior Computer Science and Linguistics undergraduate at
                                               the University of California, Los Angeles. His interests include: coding,
                                               linguistics, computational linguistics, artificial intelligence, volleyball and jazz.
                                               He expects to graduate in the Spring of 2021 and hopes to pursue a career in 
