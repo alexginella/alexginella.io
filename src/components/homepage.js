4cb82afe39e3bdfc695ae92e5b5680f47146e31c
Alex Ginella <ginellalex@gmail.com>
2020-07-24 23:42:47 -0700

    need to use local image for images or eltse!!

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import { Grid, Cell } from 'react-mdl'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 import '../App.css'
 
 class Home extends Component {
@@ -11,16 +11,15 @@ class Home extends Component {
             <div>
                 <Grid className="landing-grid">
                     <Cell col={12}>
-                        <img src="https://lh3.googleusercontent.com/vFrZjIP_JpMQvB49ogsHuNzaQBAiivdYRqo6z7bAaZfDUPlNtJZx2UVotmorpPYVkJNlgtGyVAXi2IQ5PR8-aKMKDJa6h1zkfVt4abiKAaOXaSOy1RtLcCmjAi8ctBLM1FvpyFAzWapXIW84YcsyQbK-GDUHCPJl8DwbiJY8euqL0Qzh0XLaPti3bIWmp6mASG_n3Kzt49YtN6XLRqWK45w0WtoZB2qW5zYRMcZxP42ZyqNpilTiNTTEMFto_1H66xI9egUBSOnNFJ-qyObh0eLRl8L6Qgx5MQVdXwBq2Mvd0Sz38B-jKpH5LWpkWzbxI-tkHhfSUEJmS-4ySsYWi1NUmO-Wo6M9iycrmaHqEHmcmenxd1KFJ9sEY25KMoSoiL-ayZq_L6Jbb7deIWn4Z4y0rEtzJyPvAyfWEbsip2xbh4nanHOKKHJXxi6Wp4-xraMgqOznD9Z4nM_92euK1ro7x2GzQHAwPKKYyVg_BtTh5pU2jL9pS3l3qufmBx52xeWuSbYd1cLrsJu-qPqFy_kOa57nRByf_Qh_PVPtG99lNw7AH9qnF_ZTMWPxPh7-qq4tbaS4WkbpetrmHJlOA7ZloDGCA1tGAT-RSwXmsd4h-S-PifgyIXgfJKRCSybsp2jloRdfJGi78DilyRCN2SssPQUNyjK7wD1efHCOMkKGemK1DoJsxCO8qprEvPg=s937-no?authuser=0" 
-                             alt="pic" 
+                        <img src="pfp.jpg" 
+                             alt="my face" 
                              className="pic"/>
                         <h2 className='search-bar'>
                             <FontAwesomeIcon className="search-icon" icon={faSearch} size="sm" />
                             alex ginella<span style={{opacity:".5"}}> ... computer science</span>
                         </h2>
                     </Cell>
-                    <Cell col={3}></Cell>
-                    <Cell col={6}>
+                    <Cell col={12}>
                         <div className="search-results">
                             <Link style={{textDecoration:"none"}} to="/Projects/Kontagion"><p className="intro-text">alex ginella <strong>C++</strong></p></Link>
                             <Link style={{textDecoration:"none"}} to="/Projects/aMAZEing"><p className="intro-text">alex ginella <strong>Java</strong></p></Link>
