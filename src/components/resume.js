449cd6f671acfdad6a47477fcc74bd1a9056b32b
Alex Ginella <ginellalex@gmail.com>
2020-10-23 16:48:54 -0700

    switched to updated resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -13,7 +13,7 @@ class Resume extends Component {
                         <a style={{fontSize:"20pt"}} className="title-text" target="blank_" href="https://docs.google.com/document/d/182ofZSJR9y4GEHKDQK8B3WR5QzKYTG8KA8JEl5Pm0zg/edit?usp=sharing">Shareable Link</a>
                     </Cell>
                     <Cell style={{textAlign:"center"}} col={6}>                        
-                        <iframe title="resume" style={{width:"850px", height:"900px",frameborder:"0"}} src="https://docs.google.com/document/d/e/2PACX-1vR_gP_l_4bwD66HUFoHKcHQgDUNITgYvHeU4_BGQ0YC5tWQ5DwpwyxRuaUXhUnysU6BLGPYgnRnIlW3/pub?embedded=true"></iframe>
+                        <iframe title="resume" style={{width:"850px", height:"900px",frameborder:"0"}} src="https://docs.google.com/document/d/e/2PACX-1vR9AQjRBOPV1628RY4HmghsVnDJ07NoBIudHzvy2nHuD1cuu2HEJI9LjzfiTCYqfSng1N_9GN2oQiuX/pub?embedded=true"></iframe>
                     </Cell>
                 </Grid>
             </div>
